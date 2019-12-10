531bdacecaeb3a2759c90dee431e120cb70b0356
JENNIFER SAMANTA MINAYO YUJATO <jennifer.minayo@epn.edu.ec>
2019-12-10 17:12:43 -0500

    Segunda Interfaz grafica

--- app/src/main/java/com/example/saludable/MainActivity.java
+++ app/src/main/java/com/example/saludable/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.saludable;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView postList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        drawerLayout = findViewById ( R.id.drawable_layout );
+        navigationView = findViewById ( R.id.navigation_view );
+        View navView = navigationView.inflateHeaderView ( R.layout.navigation_header );
+
+
+        navigationView.setNavigationItemSelectedListener ( new NavigationView.OnNavigationItemSelectedListener () {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSelector ( item );
+
+                return false;
+            }
+        } );
     }
+
+    private void UserMenuSelector(MenuItem item) {
+        switch (item.getItemId ()) {
+            case R.id.nav_profile:
+                Toast.makeText ( this, "Profile", Toast.LENGTH_SHORT ).show ();
+                break;
+            case R.id.nav_home:
+                Toast.makeText ( this, "Home", Toast.LENGTH_SHORT ).show ();
+                break;
+            case R.id.nav_marathon:
+                Toast.makeText ( this, "Marathon", Toast.LENGTH_SHORT ).show ();
+                break;
+            case R.id.nav_find_marathon:
+                Toast.makeText ( this, "Find Marathon", Toast.LENGTH_SHORT ).show ();
+                break;
+            case R.id.nav_message:
+                Toast.makeText ( this, "Messages", Toast.LENGTH_SHORT ).show ();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText ( this, "Settings", Toast.LENGTH_SHORT ).show ();
+                break;
+            case R.id.nav_Logout:
+                Toast.makeText ( this, "logout", Toast.LENGTH_SHORT ).show ();
+                break;
+
+        }
+
+    }
+
+
 }
