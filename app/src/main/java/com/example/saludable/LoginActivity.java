9d6b5f55d2928c6676935421e5a9de38f537df5f
JENNIFER SAMANTA MINAYO YUJATO <jennifer.minayo@epn.edu.ec>
2020-01-19 00:52:01 -0500

    Publicaciones de usuarios y Login con google.

--- app/src/main/java/com/example/saludable/LoginActivity.java
+++ app/src/main/java/com/example/saludable/LoginActivity.java
@@ -4,20 +4,30 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,6 +36,10 @@ public class LoginActivity extends AppCompatActivity {
     private TextView NeedNewAccountLink;
     private ProgressDialog loadingBar;
     private FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 1;
+    private static final String TAG = "LoginActivity";
+    private ImageView GoogleSingButton;
+    private GoogleApiClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +47,16 @@ public class LoginActivity extends AppCompatActivity {
         setContentView ( R.layout.activity_login );
 
         mAuth = FirebaseAuth.getInstance ();
+
         NeedNewAccountLink = findViewById ( R.id.register_account_link );
         UserEmail = findViewById ( R.id.login_email );
         UserPassword = findViewById ( R.id.login_password );
         LoginButton = findViewById ( R.id.login_button );
+        GoogleSingButton = findViewById ( R.id.google_signin_button );
+
+
         loadingBar = new ProgressDialog ( this );
+
 
         NeedNewAccountLink.setOnClickListener ( new View.OnClickListener () {
             @Override
@@ -53,6 +72,88 @@ public class LoginActivity extends AppCompatActivity {
                 AllowingUserToLogin ();
             }
         } );
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder ( GoogleSignInOptions.DEFAULT_SIGN_IN )
+                .requestIdToken ( getString ( R.string.default_web_client_id ) )
+                .requestEmail ()
+                .build ();
+
+        mGoogleSignInClient = new GoogleApiClient.Builder ( this ).
+                enableAutoManage ( this, new GoogleApiClient.OnConnectionFailedListener () {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText ( LoginActivity.this, "Conexión con Google fallida", Toast.LENGTH_SHORT ).show ();
+
+                    }
+                } ).addApi ( Auth.GOOGLE_SIGN_IN_API, gso ).build ();
+
+
+        GoogleSingButton.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                signIn ();
+            }
+
+        } );
+
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent ( mGoogleSignInClient );
+        startActivityForResult ( signInIntent, RC_SIGN_IN );
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult ( requestCode, resultCode, data );
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+
+            loadingBar.setTitle ( "Google sign in" );
+            loadingBar.setMessage ( "Please wait, while we are allowing you to login using your Google Account..." );
+            loadingBar.setCanceledOnTouchOutside ( true );
+            loadingBar.show ();
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent ( data );
+
+            if (result.isSuccess ()) {
+                GoogleSignInAccount account = result.getSignInAccount ();
+                firebaseAuthWithGoogle ( account );
+                Toast.makeText ( this, "Please sait, while we are getting your auth result...", Toast.LENGTH_SHORT ).show ();
+
+            } else {
+                Toast.makeText ( this, "Cant get auth result... ", Toast.LENGTH_SHORT ).show ();
+                loadingBar.dismiss ();
+            }
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d ( TAG, "firebaseAuthWithGoogle:" + acct.getId () );
+
+        AuthCredential credential = GoogleAuthProvider.getCredential ( acct.getIdToken (), null );
+        mAuth.signInWithCredential ( credential )
+                .addOnCompleteListener ( this, new OnCompleteListener<AuthResult> () {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful ()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d ( TAG, "signInWithCredential:success" );
+                            SendUserToMainActivity ();
+                            loadingBar.dismiss ();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w ( TAG, "signInWithCredential:failure", task.getException () );
+                            String message = task.getException ().toString ();
+                            SendUserToLoginActivity ();
+                            Toast.makeText ( LoginActivity.this, "Not Authenticated: " + message, Toast.LENGTH_SHORT ).show ();
+                            loadingBar.dismiss ();
+                        }
+                    }
+                } );
     }
 
     @Override
@@ -75,8 +176,9 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             loadingBar.setTitle ( "Login" );
             loadingBar.setMessage ( "Please wait, while we are allowing you to login into your Account..." );
-            loadingBar.show ();
             loadingBar.setCanceledOnTouchOutside ( true );
+            loadingBar.show ();
+
             mAuth.signInWithEmailAndPassword ( email, password )
                     .addOnCompleteListener ( new OnCompleteListener<AuthResult> () {
                         @Override
@@ -97,6 +199,13 @@ public class LoginActivity extends AppCompatActivity {
 
     private void SendUserToMainActivity() {
         Intent mainIntent = new Intent ( LoginActivity.this, MainActivity.class );
+        mainIntent.addFlags ( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );
+        startActivity ( mainIntent );
+        finish ();
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent mainIntent = new Intent ( LoginActivity.this, LoginActivity.class );
         mainIntent.addFlags ( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );
         startActivity ( mainIntent );
         finish ();
--- app/src/main/java/com/example/saludable/MainActivity.java
+++ app/src/main/java/com/example/saludable/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.saludable;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,8 +17,11 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -32,13 +40,15 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private RecyclerView postList;
     private Toolbar mToolbar;
+
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private CircleImageView NavProfileImage;
     private TextView NavProfileusername;
+    private ImageButton AddNewPostButton;
 
     private FirebaseAuth mAuth;
-    private DatabaseReference UsersRef;
-
+    private DatabaseReference UsersRef, PostRef;
+    private FirebaseRecyclerAdapter firebaseRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +58,15 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance ();
 
         UsersRef = FirebaseDatabase.getInstance ().getReference ().child ( "Users" );
+        PostRef = FirebaseDatabase.getInstance ().getReference ().child ( "Posts" );
 
 
         mToolbar = findViewById ( R.id.main_page_toolbar );
         setSupportActionBar ( mToolbar );
         getSupportActionBar ().setTitle ( "Home" );
+
+        AddNewPostButton = findViewById ( R.id.add_new_post_button );
+
 
         drawerLayout = findViewById ( R.id.drawable_layout );
         actionBarDrawerToggle = new ActionBarDrawerToggle ( MainActivity.this, drawerLayout, R.string.drawer_open, R.string.drawer_open );
@@ -60,18 +74,25 @@ public class MainActivity extends AppCompatActivity {
         actionBarDrawerToggle.syncState ();
         getSupportActionBar ().setDisplayHomeAsUpEnabled ( true );
         navigationView = findViewById ( R.id.navigation_view );
-        View navView = navigationView.inflateHeaderView ( R.layout.navigation_header );
 
+        postList = findViewById ( R.id.all_users_post_list );
+        postList.setHasFixedSize ( true );
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager ( this );
+        linearLayoutManager.setReverseLayout ( true );
+        linearLayoutManager.setStackFromEnd ( true );
+
+        postList.setLayoutManager ( linearLayoutManager );
+
+
+        View navView = navigationView.inflateHeaderView ( R.layout.navigation_header );
         NavProfileImage = navView.findViewById ( R.id.nav_profile_image );
         NavProfileusername = navView.findViewById ( R.id.nav_user_full_name );
-
-
 
         navigationView.setNavigationItemSelectedListener ( new NavigationView.OnNavigationItemSelectedListener () {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 UserMenuSelector ( item );
-
                 return false;
             }
         } );
@@ -88,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
             SendUserTologinActivity ();
         } else {
             CheckUserExistence ();
+            firebaseRecyclerAdapter.startListening ();
         }
     }
 
@@ -104,11 +126,18 @@ public class MainActivity extends AppCompatActivity {
                     if (!dataSnapshot.child ( current_user_id ).hasChild ( "username" )) {
                         SendUserToSetupActivity ();
                     } else {
-                        String fullname = dataSnapshot.child ( current_user_id ).child ( "fullname" ).getValue ().toString ();
-                        String image = dataSnapshot.child ( current_user_id ).child ( "profileimage" ).getValue ().toString ();
+                        if (dataSnapshot.child ( current_user_id ).hasChild ( "fullname" )) {
+                            String fullname = dataSnapshot.child ( current_user_id ).child ( "fullname" ).getValue ().toString ();
+                            NavProfileusername.setText ( fullname );
 
-                        NavProfileusername.setText ( fullname );
-                        Picasso.with ( MainActivity.this ).load ( image ).placeholder ( R.drawable.profile ).into ( NavProfileImage );
+                        }
+                        if (dataSnapshot.child ( current_user_id ).hasChild ( "profileimage" )) {
+                            String image = dataSnapshot.child ( current_user_id ).child ( "profileimage" ).getValue ().toString ();
+
+                            Picasso.with ( MainActivity.this ).load ( image ).placeholder ( R.drawable.profile ).into ( NavProfileImage );
+                        } else {
+                            Toast.makeText ( MainActivity.this, "El usuario no tiene foto de perfil", Toast.LENGTH_SHORT ).show ();
+                        }
                     }
                 }
             }
@@ -118,34 +147,63 @@ public class MainActivity extends AppCompatActivity {
 
             }
         } );
-    }
 
-    private void SendUserToSetupActivity() {
-        Intent setupIntent = new Intent ( MainActivity.this, SetupActivity.class );
-        setupIntent.addFlags ( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );
-        startActivity ( setupIntent );
-        finish ();
-    }
+        AddNewPostButton.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                SendUserToPostActivity ();
+            }
+        } );
 
 
-    private void SendUserTologinActivity() {
-        Intent loginIntent = new Intent ( MainActivity.this, LoginActivity.class );
-        loginIntent.addFlags ( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );
-        startActivity ( loginIntent );
-        finish ();
+        DisplayAllUsersPosts ();
     }
 
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (actionBarDrawerToggle.onOptionsItemSelected ( item )) {
-            return true;
-        }
-        return super.onOptionsItemSelected ( item );
+    protected void DisplayAllUsersPosts() {
+
+
+        FirebaseRecyclerOptions<Post> options = new FirebaseRecyclerOptions.Builder<Post> ()
+                .setQuery ( PostRef, Post.class ).build ();
+
+        firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Post, PostViewHolder> ( options ) {
+                    @Override
+                    public PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from ( parent.getContext () )
+                                .inflate ( R.layout.all_post_layout, parent, false );
+                        return new PostViewHolder ( view );
+                    }
+
+                    @Override
+                    protected void onBindViewHolder(PostViewHolder postViewHolder, int i, @NonNull Post post) {
+                        postViewHolder.setFullname ( post.getFullname () );
+                        postViewHolder.setTime ( post.getTime () );
+                        postViewHolder.setDate ( post.getDate () );
+                        postViewHolder.setDescription ( post.getDescription () );
+                        postViewHolder.setProfileimage ( getApplicationContext (), post.getProfileimage () );
+                        postViewHolder.setPostImage ( getApplicationContext (), post.getPostimage () );
+
+                    }
+
+                };
+
+        postList.setAdapter ( firebaseRecyclerAdapter );
+
+
+    }
+
+    private void SendUserToPostActivity() {
+
+        Intent addNewPostIntent = new Intent ( MainActivity.this, PostActivity.class );
+        startActivity ( addNewPostIntent );
     }
 
     private void UserMenuSelector(MenuItem item) {
         switch (item.getItemId ()) {
+            case R.id.nav_post:
+                SendUserToPostActivity ();
+                break;
             case R.id.nav_profile:
                 Toast.makeText ( this, "Profile", Toast.LENGTH_SHORT ).show ();
                 break;
@@ -173,5 +231,70 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void SendUserToSetupActivity() {
+        Intent setupIntent = new Intent ( MainActivity.this, SetupActivity.class );
+        setupIntent.addFlags ( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );
+        startActivity ( setupIntent );
+        finish ();
+    }
 
+
+    private void SendUserTologinActivity() {
+        Intent loginIntent = new Intent ( MainActivity.this, LoginActivity.class );
+        loginIntent.addFlags ( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );
+        startActivity ( loginIntent );
+        finish ();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected ( item )) {
+            return true;
+        }
+        return super.onOptionsItemSelected ( item );
+    }
+
+    public class PostViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+        Context mContext;
+
+        public PostViewHolder(View itemView) {
+            super ( itemView );
+            mView = itemView;
+            mContext = itemView.getContext ();
+        }
+
+        public void setFullname(String fullname) {
+            TextView username = mView.findViewById ( R.id.post_profile_name );
+            username.setText ( fullname );
+        }
+
+        public void setProfileimage(Context ctx, String profileimage) {
+            CircleImageView image = mView.findViewById ( R.id.post_profile_image );
+            Picasso.with ( ctx ).load ( profileimage ).into ( image );
+
+        }
+
+        public void setTime(String time) {
+            TextView postTime = mView.findViewById ( R.id.post_time );
+            postTime.setText ( time );
+        }
+
+        public void setDate(String date) {
+            TextView postDate = mView.findViewById ( R.id.post_date );
+            postDate.setText ( date );
+        }
+
+        public void setDescription(String description) {
+            TextView postDescription = mView.findViewById ( R.id.post_description );
+            postDescription.setText ( description );
+        }
+
+        public void setPostImage(Context ctx, String postImage) {
+            ImageView image = mView.findViewById ( R.id.post_image );
+            Picasso.with ( ctx ).load ( postImage ).into ( image );
+        }
+
+    }
 }
